643654fca425df5b9f8ffe87a194887b41f33485
Agop Chorbadjian <101152834+PowChorba@users.noreply.github.com>
2023-10-27 14:05:30 -0300

    Update page.tsx

--- robotize-front/app/page.tsx
+++ robotize-front/app/page.tsx
@@ -11,13 +11,13 @@ export default function App() {
   useEffect(() => {
     setTimeout(() => {
       router.push('/home')
-    }, 500)
+    }, 250)
   },[router])
 
   return (
     <div className="h-[100vh] flex flex-col justify-center items-center gap-10">
       <h2 className="text-4xl font-bold text-[#441eae]">Noticias en Robotize</h2>
-      <DotWave size={47} speed={1} color="black" />
+{/*       <DotWave size={47} speed={1} color="black" /> */}
     </div>
   );
 }
