dd924a8bd40ab6a7a04cddad2f420296ffb12ba2
PowChorba <101152834+PowChorba@users.noreply.github.com>
2023-11-09 01:39:21 -0300

    revalidate for requests to DB

--- robotize-front/app/admin/page.tsx
+++ robotize-front/app/admin/page.tsx
@@ -1,6 +1,7 @@
 import Admin from "@/components/Admin/Admin";
 import { getAllAdmin } from "./service/admin.service";
 
+export const revalidate = 200;
 
 export default async function AdminApp(){
     const data = await getAllAdmin('1')
