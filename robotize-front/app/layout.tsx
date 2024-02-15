4c326845739bdc682d5cebdae625a0355996a917
Agop Chorbadjian <101152834+PowChorba@users.noreply.github.com>
2024-02-15 17:34:39 -0300

    chA

--- robotize-front/app/layout.tsx
+++ robotize-front/app/layout.tsx
@@ -2,7 +2,7 @@ import Providers from '@/components/Provider/Provider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Roboto, Roboto_Mono } from 'next/font/google'
-
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Noticias Robotize',
@@ -39,8 +39,8 @@ export default function RootLayout({
             }}
           />
       </head>
-      <body className={roboto_mono.className}><Providers><script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
-      <script src="https://mediafiles.botpress.cloud/4ff505f2-01cd-418e-b4b6-0f87564fecb8/webchat/config.js" defer></script>{children}</Providers></body>
+      <body className={roboto_mono.className}><Providers><Script src="https://cdn.botpress.cloud/webchat/v1/inject.js" strategy="afterInteractive" />
+          <Script src="https://mediafiles.botpress.cloud/4ff505f2-01cd-418e-b4b6-0f87564fecb8/webchat/config.js" strategy="afterInteractive" />{children}</Providers></body>
     </html>
   )
 }
